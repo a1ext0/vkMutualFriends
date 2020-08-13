c25194b35bbabfa8886433b90f1b41dd627802d3
sto <sto@MacBook-Pro-Aleksej.local>
2020-08-13 14:28:42 +0300

    tests counter

--- src/lib/counter.ts
+++ src/lib/counter.ts
@@ -6,21 +6,25 @@ class Counter {
     starts:number = 0
     max:number = 20
     i = 400
+    interval:NodeJS.Timeout
 
-    init():void {
-        setInterval(() => {
+    constructor() {
+        this.interval = setInterval(() => {
             this.starts = 0
             eventEmitter.emit('allowStart')
         }, this.i);
     }
+    stop():void {
+        clearInterval(this.interval)
+    }
 
     async start():Promise<boolean> {
-        if (this.starts < this.max) {
+        if (this.starts < (this.max-1)) {
             this.starts++
             return true
         } else {
             return new Promise((resolve, reject)=> {
-                eventEmitter.on('allowStart', ()=> {
+                eventEmitter.once('allowStart', ()=> {
                     resolve(true)
                 })
             })
---
+++ src/test/counter.test.ts
@@ -0,0 +1,17 @@
+import counter from '../lib/counter'
+
+import assert from 'assert'
+
+describe('Counter', function () {
+    this.timeout(10000)
+    describe('#start()', ()=> {
+        it('should return Promise', async ()=> {
+            for (let i = 0; i < 400; i++) {
+                let res = await counter.start()
+                console.log(i);
+                assert.equal(true, res)
+            }
+            counter.stop()
+        })
+    })
+})
