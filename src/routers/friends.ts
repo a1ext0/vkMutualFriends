3237d378937306fb532dab4c02ff83efdc23dd97
sto <sto@MacBook-Pro-Aleksej.local>
2020-08-14 20:57:57 +0300

    Обработчик post запроса

--- src/routers/friends.ts
+++ src/routers/friends.ts
@@ -1,4 +1,5 @@
 import Router from 'koa-router';
+import friends from '../lib/friends'
 const router = new Router();
 
 router.post('/friends', async (ctx) => {
@@ -7,7 +8,29 @@ router.post('/friends', async (ctx) => {
             let id1 = ctx.request.body.id1
             let id2 = ctx.request.body.id2
             if (typeof id1 == 'number' && typeof id2 == 'number') {
+                let mutualFriends = await await friends.recCompare(239585468, 510530939)
+                switch (mutualFriends) {
+                    case true:
+                        ctx.body = { status: 1, about: 'Эти пользователи друзья' }
+                        break;
+
+                    case false:
+                        ctx.body = { status: 0, about: 'Запрос отклонён, проверьте id пользователей' }
+                        break;
                 
+                    default:
+                        if (Array.isArray(mutualFriends)) {
+                            if (mutualFriends.length == 0) {
+                                ctx.body = { status: 2, about: 'У этих пользователей не найдено общих друзей с открытыми профилями' }
+                            } else {
+                                ctx.body = { status: 3, friends: mutualFriends, about: `У этих пользователей есть ${mutualFriends.length} общих друзей` }
+                            }
+                        } else {
+                            ctx.status = 500
+                            ctx.body = { error: 'Ошибка сервера' }
+                        }
+                        break;
+                }
             } else {
                 ctx.status = 422
                 ctx.body = {error: 'Поля "id1" и "id2" должны быть числами'}
